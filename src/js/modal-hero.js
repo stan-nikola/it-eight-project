9075f9181515ee4f4493c4ab2c328a38552e9bb3
KarinaShtankova <shtankovads@gmail.com>
2022-08-10 16:33:41 +0300

    add modal-window-js

--- src/js/modal-hero.js
+++ src/js/modal-hero.js
@@ -1,15 +1,19 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open-hero]"),
+    openModalBtn: document.querySelectorAll("[data-modal-open-hero]"),
     closeModalBtn: document.querySelector("[data-modal-close-hero]"),
     modal: document.querySelector("[data-modal-hero]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.forEach(el => el.addEventListener("click", toggleModal));
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
     document.body.classList.toggle("modal-open");
     refs.modal.classList.toggle("is-hidden");
+
+    if (refs.modal.classList.contains("is-hidden")) {
+      document.body.dispatchEvent(new Event("closeSideNav"))
+    }
   }
 })();
