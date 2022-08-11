7ded91a5c9b9698ebda6c3db790adfff3bb626f4
Maksym Shvets <maxim27061993@outlook.com>
2022-08-11 19:56:03 +0200

    customer-reviews final

--- src/js/slick.js
+++ src/js/slick.js
@@ -1,6 +1,8 @@
 $('.single-item').slick({
     dots: true,
     arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
     customPaging: function () {
         return '<div class="custom-slick-dot"></div>';
     },
